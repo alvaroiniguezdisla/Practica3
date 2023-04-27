40d31ccd398cb090bc85798b497cb6102f394dca
Álvaro <alvaro.iniguezdisla@usp.ceu.es>
2023-04-27 22:03:44 +0200

    Practica terminada

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,71 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+import pr2.Graph;
+
+public class Principal {
+    public static void main(String[] args) {
+        Graph<String> g = new Graph<>();
+        g.addVertex("A");
+        g.addVertex("B");
+        g.addVertex("C");
+        g.addVertex("D");
+        g.addVertex("E");
+        g.addVertex("F");
+        g.addVertex("G");
+        g.addVertex("H");
+        g.addVertex("I");
+        g.addVertex("J");
+        g.addVertex("K");
+        g.addVertex("L");
+        g.addVertex("M");
+        g.addVertex("N");
+        g.addVertex("O");
+        g.addVertex("P");
+        g.addVertex("Q");
+        g.addVertex("R");
+        g.addVertex("S");
+        g.addVertex("T");
+        g.addVertex("U");
+        g.addVertex("V");
+        g.addVertex("W");
+        g.addVertex("X");
+        g.addVertex("Y");
+        g.addVertex("Z");
+        g.aadEdge("A", "B");
+        g.aadEdge("A", "C");
+        g.aadEdge("A", "D");
+        g.aadEdge("A", "E");
+        g.aadEdge("A", "F");
+        g.aadEdge("A", "G");
+        g.aadEdge("A", "H");
+        g.aadEdge("A", "I");
+        g.aadEdge("A", "J");
+        g.aadEdge("A", "K");
+        g.aadEdge("A", "L");
+        g.aadEdge("A", "M");
+        g.aadEdge("A", "N");
+        g.aadEdge("A", "O");
+        g.aadEdge("A", "P");
+        g.aadEdge("A", "Q");
+        g.aadEdge("A", "R");
+        g.aadEdge("A", "S");
+        g.aadEdge("A", "T");
+        g.aadEdge("A", "U");
+        g.aadEdge("A", "V");
+        g.aadEdge("A", "W");
+        g.aadEdge("A", "X");
+        g.aadEdge("A", "Y");
+        g.aadEdge("A", "Z");
+        System.out.println(g);
+        System.out.println(g.toString());
+        System.out.println(g.containsVertex("A"));
+        System.out.println(g.containsVertex("B"));
+
+
+
+
+
+
+
+    }
 }
--- src/pr2/Graph.java
+++ src/pr2/Graph.java
@@ -1,7 +1,99 @@
-package dominio;
-
+package pr2;
+import java.lang.reflect.Array;
+import java.util.*;
 public class Graph<V>{
-    
+    private Map<V, Set<V>> adjacencyList = new HashMap<>();//vector,conjunto de adyacentes a ese vector
+    public boolean addVertex(V v){
+        if(adjacencyList.containsKey(v)){
+            return false;
+        }else{
+            adjacencyList.put(v,new HashSet<V>());
+            return true;
+        }
+    }
+    public boolean aadEdge(V v1,V v2) {
+
+        if (!adjacencyList.containsKey(v1)) {
+            adjacencyList.put(v1, new HashSet<V>());
 
 
-}
+        }
+        if (!adjacencyList.containsKey(v2)) {
+            adjacencyList.put(v2, new HashSet<V>());
+
+        }
+
+        adjacencyList.get(v1).add(v2);
+        adjacencyList.get(v2).add(v1);
+        return true;
+    }
+
+    public Set<V> obtainAdjacents(V v) throws Exception {
+        if (!adjacencyList.containsKey(v)) {
+
+            throw new Exception("No existe el vertice");
+        }
+
+        return adjacencyList.get(v);
+
+
+    }
+    public boolean containsVertex(V v){
+        if(adjacencyList.containsKey(v)){
+            return true;
+        }
+        return false;
+    }
+
+    public String toString(){
+
+
+        return adjacencyList.toString();
+
+
+    }
+    public static <V> void decodificar(V v1,V v2,Map<V,V> traza){
+        Stack<V> pila = new Stack<>();
+        pila.push(v2);
+        V actual = v2;
+        while(!actual.equals(v1)){
+            actual = traza.get(actual);
+            pila.push(actual);
+        }
+        while(!pila.isEmpty()){
+            System.out.println(pila.pop());
+        }
+    }
+    public Map<V,V> onePath(V v1, V v2){
+        Set<V> pisados = new HashSet<>();
+        Map<V, V> traza = new HashMap<>();
+        Stack abierta = new Stack();
+        abierta.push(v1);
+        traza.put(v1,v1);
+        boolean encontrado = false;
+
+        while(!abierta.isEmpty() && !encontrado){
+            V actual=(V)abierta.pop();
+            pisados.add(actual);
+            if(actual.equals(v2)) {
+                encontrado = true;
+            }
+            if(encontrado==false){
+                for(V ady:adjacencyList.get(actual)){
+                    if(!pisados.contains(ady)){
+                        abierta.push(ady);
+                        traza.put(ady, actual);
+                     }
+                }
+            }
+
+        }
+        return traza;
+
+
+
+    }
+
+
+
+    }
